eb9820e2926f8bc82a99acf45ea48b45c6014e46
ASAZING <malcolmedina206@gmail.com>
2024-09-05 22:24:26 -0500

    feat(index): Agrego index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <header>
+        <div id="menu">
+            <ul>
+                <li><a href="#">WeatherApp</a></li>
+                <li><a href="views/signup.php">Regístrate</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <div class="container">
+        <h2>Bienvenido</h2>
+        <p>Inicia sesión o <a href="views/login.php">Login</a> o <a href="views/signup.php">Regístrate</a></p>
+    </div>
+
+</body>
+
+</html>
